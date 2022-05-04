485e298325aace8d81f191c0d017d57a14aea761
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-04 10:34:36 +0200

    Temp fix integration test - bookmark-listing

--- tests/integration/components/bookmark-listing-test.js
+++ tests/integration/components/bookmark-listing-test.js
@@ -1,18 +1,21 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('bookmark-listing', 'Integration | Component | bookmark listing', {
-  integration: true
-});
+moduleForComponent(
+  "bookmark-listing",
+  "Integration | Component | bookmark listing",
+  {
+    integration: true,
+  }
+);
 
-test('it renders', function(assert) {
-
+test("it renders", function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{bookmark-listing}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), "-");
 
   // Template block usage:
   this.render(hbs`
@@ -21,5 +24,13 @@ test('it renders', function(assert) {
     {{/bookmark-listing}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(
+    this.$().text().trim(),
+    `-
+    
+  
+
+
+      template block text`
+  );
 });
