9a00b24cb8edcb0d8e38a2e9dd82dcc7d65d6b19
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-04 12:01:15 +0200

    Add test for helper page-title

---
+++ tests/unit/helpers/page-title-test.js
@@ -0,0 +1,10 @@
+import { pageTitle } from "bookmarker/helpers/page-title";
+import { module, test } from "qunit";
+
+module("Unit | Helper | page title");
+
+// Replace this with your real tests.
+test("it works", function (assert) {
+  let result = pageTitle([42]);
+  assert.notOk(result);
+});
