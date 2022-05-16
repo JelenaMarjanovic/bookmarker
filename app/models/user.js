3c5bb6956d81e9d718f0bb88b05271d56deade09
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-16 13:16:17 +0200

    Computed properties - user, fullName

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 import DS from "ember-data";
+import Ember from "ember";
 
 export default DS.Model.extend({
   username: DS.attr("string"),
@@ -13,4 +14,7 @@ export default DS.Model.extend({
     },
   }),
   bookmarks: DS.hasMany("bookmark"),
+  fullName: Ember.computed("firstName", "lastName", function () {
+    return `${this.get("firstName")} ${this.get("lastName")}`;
+  }),
 });
