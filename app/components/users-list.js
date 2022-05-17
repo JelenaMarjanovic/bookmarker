9106a23511d73fabc6f623dd38613aed5c75d65a
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-17 16:41:42 +0200

    Component event - users-list click

--- app/components/users-list.js
+++ app/components/users-list.js
@@ -2,6 +2,10 @@ import Ember from "ember";
 
 const UsersListComponent = Ember.Component.extend({
   imgClass: "avatar",
+  click() {
+    Ember.Logger.info("users-list was clicked");
+    return false;
+  },
 });
 
 UsersListComponent.reopenClass({
