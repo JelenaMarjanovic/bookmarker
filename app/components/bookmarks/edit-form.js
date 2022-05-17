3d25eff8ca567946f1b6b767f48d389c623f3ba7
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-17 23:38:28 +0200

    Build a basic form - new/edit bookmark form

---
+++ app/components/bookmarks/edit-form.js
@@ -0,0 +1,12 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+  actions: {
+    save(bookmark) {
+      Ember.Logger.info("save called");
+    },
+    cancel() {
+      Ember.Logger.info("cancel called");
+    },
+  },
+});
