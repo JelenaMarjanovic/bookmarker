f5e4871833edc36e575a4fa24d8abc551b3925f4
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-19 23:41:19 +0200

    Add integration component test - users-list

--- tests/integration/components/users-list-test.js
+++ tests/integration/components/users-list-test.js
@@ -1,18 +1,17 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('users-list', 'Integration | Component | users list', {
-  integration: true
+moduleForComponent("users-list", "Integration | Component | users list", {
+  integration: true,
 });
 
-test('it renders', function(assert) {
-
+test("it renders", function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{users-list}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), "");
 
   // Template block usage:
   this.render(hbs`
@@ -21,5 +20,23 @@ test('it renders', function(assert) {
     {{/users-list}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), "template block text");
+});
+
+test("verify that the image class is the correct value", function (assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{users-list}} fake-url sample-email`);
+
+  assert.equal(this.$("img").attr("class"), "avatar");
+
+  // Template block usage:
+  this.render(hbs`
+    {{#users-list fake-url sample-email}}
+      template block text
+    {{/users-list}}
+  `);
+
+  assert.equal(this.$("img").attr("class"), "avatar");
 });
